9c1649327d612113361c987652f3b462c2a27a4e
Venelin Manchev <manchev@prodio.bg>
2018-03-22 12:43:28 +0200

    feat(ng): angular setup with dummy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DriversComponent } from './drivers/drivers.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ConstructorsComponent } from './constructors/constructors.component';
+import { CircuitsComponent } from './circuits/circuits.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DriversComponent,
+    NavComponentComponent,
+    ProfileComponent,
+    ConstructorsComponent,
+    CircuitsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
